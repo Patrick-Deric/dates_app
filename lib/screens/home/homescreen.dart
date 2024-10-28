1c4431d47963ad4350a70702edb6296defc52e57
Patrick-Deric <patrick.deric@gmail.com>
2024-10-28 12:10:54 -0300

    28/10/24

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -1,16 +1,16 @@
-import 'package:app_de_dates/screens/home/criar_rota.dart';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart' as permHandler;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../auth/login_page.dart';
-import 'favourites.dart';
-import 'profile.dart';
-import '/widgets/mapbox_map_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:geolocator/geolocator.dart';  // Added Geolocator for user location
 import 'package:flutter/services.dart';
 import 'dart:typed_data';
+
+import 'criar_rota.dart';
+import 'favourites.dart';
+import 'profile.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -21,47 +21,47 @@ class _HomeScreenState extends State<HomeScreen> {
   bool _locationPermissionGranted = false;
   int _selectedIndex = 0;
   String _mapStyle = MapboxStyles.MAPBOX_STREETS;
-  LatLng initialPosition = LatLng(-23.5505, -46.6333); // São Paulo coordinates by default
+  LatLng initialPosition = LatLng(-23.5505, -46.6333); // Default coordinates
+  LatLng? userPosition;
   MapboxMapController? _mapController;
-  List<Map<String, dynamic>> routes = []; // Store fetched routes
+  List<Map<String, dynamic>> routes = [];
 
   @override
   void initState() {
     super.initState();
     _requestLocationPermission();
-    _fetchRoutes();  // Fetch routes from Firestore
+    _fetchRoutes();
   }
 
-  // Fetch the user's current location and set initial map position
   Future<void> _requestLocationPermission() async {
-    PermissionStatus status = await Permission.location.status;
+    var status = await permHandler.Permission.location.status;
+    if (status.isDenied) {
+      status = await permHandler.Permission.location.request();
+    }
     if (status.isGranted) {
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-
       setState(() {
         _locationPermissionGranted = true;
-        initialPosition = LatLng(position.latitude, position.longitude);  // Set initial position to user's location
       });
-    } else if (status.isDenied || status.isPermanentlyDenied) {
-      final result = await Permission.location.request();
-      if (result.isGranted) {
-        Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-
-        setState(() {
-          _locationPermissionGranted = true;
-          initialPosition = LatLng(position.latitude, position.longitude);  // Set initial position to user's location
-        });
-      } else {
-        print('Location permission denied');
-      }
+      await _updateUserPosition();
     }
   }
 
-  // Fetch routes from Firestore and store them
+  Future<void> _updateUserPosition() async {
+    LatLng currentLocation = await _getCurrentLocationFromMapbox();
+    setState(() {
+      userPosition = currentLocation;
+      initialPosition = currentLocation;
+    });
+  }
+
+  Future<LatLng> _getCurrentLocationFromMapbox() async {
+    // Replace this with actual location retrieval code.
+    return LatLng(-23.5515, -46.6334);
+  }
+
   Future<void> _fetchRoutes() async {
     try {
       QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('routes').get();
-
       setState(() {
         routes = querySnapshot.docs.map((doc) {
           Map<String, dynamic> routeData = doc.data() as Map<String, dynamic>;
@@ -69,13 +69,12 @@ class _HomeScreenState extends State<HomeScreen> {
           return routeData;
         }).toList();
       });
-      print('Routes fetched successfully');
+      _addRouteIcons();
     } catch (e) {
       print('Error fetching routes: $e');
     }
   }
 
-  // Ensure custom heart icon is loaded
   Future<void> _loadCustomHeartIcon() async {
     try {
       ByteData byteData = await rootBundle.load('assets/map_icons/heart.png');
@@ -86,26 +85,23 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  // Add heart icon for each route at the first stop
   void _addRouteIcons() async {
-    if (_mapController == null) return; // Make sure map controller is initialized
-
-    // Load custom heart icon before adding symbols
+    if (_mapController == null) return;
     await _loadCustomHeartIcon();
-
     for (var route in routes) {
       if (route['stops'].isNotEmpty) {
-        final firstStop = route['stops'][0]; // First stop of the route
+        final firstStop = route['stops'][0];
         final lat = firstStop['lat'];
         final lng = firstStop['lng'];
-
         if (lat != null && lng != null) {
           try {
-            _mapController?.addSymbol(SymbolOptions(
-              geometry: LatLng(lat, lng),
-              iconImage: 'heart-icon',  // Use loaded heart icon
-              iconSize: 2.0,
-            ));
+            _mapController?.addSymbol(
+              SymbolOptions(
+                geometry: LatLng(lat, lng),
+                iconImage: 'heart-icon',
+                iconSize: 2.0,
+              ),
+            );
           } catch (e) {
             print('Error adding symbol: $e');
           }
@@ -114,9 +110,18 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  // Handle tap on the heart icon to show the custom-designed box with route info
+  double _calculateDistance(LatLng pos1, LatLng pos2) {
+    const p = 0.017453292519943295;
+    final a = 0.5 -
+        cos((pos2.latitude - pos1.latitude) * p) / 2 +
+        cos(pos1.latitude * p) * cos(pos2.latitude * p) *
+            (1 - cos((pos2.longitude - pos1.longitude) * p)) / 2;
+    return 12742 * asin(sqrt(a)); // Distance in km
+  }
+
   void _onIconTapped(Map<String, dynamic> route) {
     if (route.isNotEmpty) {
+      _drawRouteOnMap(route['stops']);
       showModalBottomSheet(
         context: context,
         builder: (context) {
@@ -142,21 +147,14 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ),
                   SizedBox(height: 10),
-                  // Display route stops
                   Expanded(
                     child: ListView.builder(
                       itemCount: route['stops'].length,
                       itemBuilder: (context, index) {
                         var stop = route['stops'][index];
                         return ListTile(
-                          title: Text(
-                            stop['name'],
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          subtitle: Text(
-                            'Lat: ${stop['lat']}, Lng: ${stop['lng']}',
-                            style: TextStyle(color: Colors.white70),
-                          ),
+                          title: Text(stop['name'], style: TextStyle(color: Colors.white)),
+                          subtitle: Text('Lat: ${stop['lat']}, Lng: ${stop['lng']}', style: TextStyle(color: Colors.white70)),
                         );
                       },
                     ),
@@ -167,35 +165,65 @@ class _HomeScreenState extends State<HomeScreen> {
           );
         },
       );
-    } else {
-      print('No route information available.');
     }
   }
 
-  // Map creation callback
+  void _drawRouteOnMap(List<dynamic> stops) {
+    _mapController?.clearSymbols();
+    for (var stop in stops) {
+      _mapController?.addSymbol(SymbolOptions(
+        geometry: LatLng(stop['lat'], stop['lng']),
+        iconImage: 'heart-icon',
+        iconSize: 1.5,
+      ));
+    }
+  }
+
+  List<Widget> _buildNearbyRoutesCards() {
+    if (routes.isEmpty || userPosition == null) return [];
+
+    return routes.where((route) {
+      final firstStop = route['stops'][0];
+      final distance = _calculateDistance(
+        userPosition!, // Use actual user position
+        LatLng(firstStop['lat'], firstStop['lng']),
+      );
+      return distance <= 15;
+    }).map((route) {
+      var duration = (route['duration'] ?? 0) / 60;
+      return Card(
+        margin: EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 3,
+        child: ListTile(
+          leading: Icon(Icons.date_range, color: Colors.redAccent),
+          title: Text(route['stops'][0]['name']),
+          subtitle: Text(
+            'Stops: ${route['stops'].length}, Duration: ${duration.toStringAsFixed(0)} mins',
+          ),
+          trailing: Icon(Icons.arrow_forward),
+          onTap: () => _onIconTapped(route),
+        ),
+      );
+    }).toList();
+  }
+
   void _onMapCreated(MapboxMapController controller) {
     _mapController = controller;
-    _addRouteIcons();  // Add heart icons for routes
-
-    // Set up the symbol tap listener
+    _addRouteIcons();
     _mapController?.onSymbolTapped.add((symbol) {
       final geometry = symbol.options.geometry;
       if (geometry != null) {
-        // Find the route associated with the tapped symbol (heart icon)
         for (var route in routes) {
           final firstStop = route['stops'][0];
           if (firstStop['lat'] == geometry.latitude && firstStop['lng'] == geometry.longitude) {
-            _onIconTapped(route);  // Show route info in the custom box
-            break; // Stop the loop once the route is found
+            _onIconTapped(route);
+            break;
           }
         }
       }
     });
   }
-
-
-
-
 
   Widget _buildSearchBar() {
     return Container(
@@ -215,41 +243,43 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _buildCategories() {
-    final categories = ["Romantic", "Family", "Cultural", "First Date"];
-    String? _selectedCategory;
-
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: categories.map((category) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ChoiceChip(
-              label: Text(category),
-              selected: _selectedCategory == category,
-              selectedColor: Theme.of(context).primaryColor,
-              labelStyle: TextStyle(
-                color: _selectedCategory == category ? Colors.white : Colors.black,
-              ),
-              onSelected: (bool isSelected) {
-                setState(() {
-                  _selectedCategory = isSelected ? category : null;
-                });
-              },
-            ),
-          );
-        }).toList(),
-      ),
-    );
+  Future<void> _logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    } catch (e) {
+      print('Error logging out: $e');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Erro ao fazer logout.'),
+      ));
+    }
   }
 
-  // Logout function
-  Future<void> _logout() async {
-    await FirebaseAuth.instance.signOut();
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => LoginPage()),
+  Widget _buildCategories() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: ["Romantic", "Family", "Cultural", "First Date"].map((category) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ChoiceChip(
+                  label: Text(category),
+                  selected: false,
+                  selectedColor: Theme.of(context).primaryColor,
+                  labelStyle: TextStyle(color: Colors.black),
+                  onSelected: (_) {},
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+        ..._buildNearbyRoutesCards(),
+      ],
     );
   }
 
@@ -258,16 +288,17 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         _locationPermissionGranted
             ? MapboxMap(
-          accessToken: 'sk.eyJ1IjoicGF0cmlja2RlcmljIiwiYSI6ImNtMmo1bHY4ZDAxemoya3B4eWdjYjd4bjYifQ',
+          accessToken: 'sk.eyJ1IjoicGF0cmlja2RlcmljIiwiYSI6ImNtMmo1bHY4ZDAxemoya3B4eWdjYjd4bjYifQ.ie9qwYOo7bEjjNFiAGxp2g',
           styleString: _mapStyle,
           initialCameraPosition: CameraPosition(
             target: initialPosition,
             zoom: 12,
           ),
           onMapCreated: _onMapCreated,
+          myLocationEnabled: true,
+          myLocationTrackingMode: MyLocationTrackingMode.Tracking,
         )
             : Center(child: CircularProgressIndicator()),
-
         DraggableScrollableSheet(
           initialChildSize: 0.4,
           minChildSize: 0.2,
@@ -293,25 +324,16 @@ class _HomeScreenState extends State<HomeScreen> {
                       color: Colors.grey[300],
                     ),
                     SizedBox(height: 20),
-
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 20.0),
                       child: _buildSearchBar(),
                     ),
                     SizedBox(height: 10),
-
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 10.0),
                       child: _buildCategories(),
                     ),
                     SizedBox(height: 10),
-
-                    Container(
-                      height: 200,
-                      child: Center(
-                        child: Text("Browse date ideas here"),
-                      ),
-                    ),
                   ],
                 ),
               ),
@@ -428,6 +450,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
-
-
